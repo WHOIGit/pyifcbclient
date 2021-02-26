4f29d49f736156586f86acc9b2afcecec9417e7e
Ryan Govostes <rgovostes@whoi.edu>
2021-02-26 14:12:14 -0500

    Add event dispatch to client

--- ifcbclient/ifcbclient.py
+++ ifcbclient/ifcbclient.py
@@ -9,9 +9,11 @@ from .protocol import parse_response
 
 
 class IFCBClient:
-    def __init__(self, server, id, autoconnect=True):
+    def __init__(self, server, id):
         self.server_url = server
         self.ifcb_id = id
+
+        self.handlers = []
 
         self.hub_connection = (
             HubConnectionBuilder()
@@ -33,9 +35,6 @@ class IFCBClient:
         self.hub_connection.on("messageRelayed", self.handle_message)
         self.hub_connection.on("startedAsClient", self.started)
         self.hub_connection.on("disconnect", self.disconnect)
-
-        if autoconnect:
-            self.connect()
 
     def connect(self):
         self.hub_connection.start()
@@ -60,7 +59,26 @@ class IFCBClient:
     def handle_message(self, response):
         sender_id, smsgsrc, seqno, msg = response
         try:
-            print(msg, parse_response(msg))
+            fields = parse_response(msg)
+            for pattern, callback in self.handlers:
+                for field, value in zip(fields, pattern):
+                    if value is None:
+                        continue
+                    elif field != value:
+                        break
+                else:
+                    # Reached if all fields match the pattern
+                    callback(*fields)
         except:
-            # signalrcore consumes all exceptions, so we need to catch it
+            # signalrcore consumes all exceptions, so we need to catch it here
+            # to log it
             traceback.print_exc()
+
+    def on(self, prefix_pattern, callback):
+        '''
+        Registers a callback to be invoked when a message arrives that begins
+        with a field sequence that matches the given pattern.
+
+        A pattern is a sequence of constants and None. None matches any value.
+        '''
+        self.handlers.append((prefix_pattern, callback))
