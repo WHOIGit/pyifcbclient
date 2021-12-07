a0ca76737eb97da0ab8358456ab0d27b856dec3a
Ryan Govostes <rgovostes@whoi.edu>
2021-12-07 16:52:08 -0500

    Fix my e-mail address

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="1.0",
     description="Interface to the McLane Imaging FlowCytobot websocket API",
     author="Ryan Govostes",
-    author_email="rovostes@whoi.edu",
+    author_email="rgovostes@whoi.edu",
     url="https://github.com/WHOIGit/pyifcbclient",
     packages=["ifcbclient"],
     install_requires=["signalrcore"],
